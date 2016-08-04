baffebdf760e1259534f82f429ac905c2e29365a
Luqman Mohamed <lukas.woodman@gmail.com>
2016-08-04 14:21:33 +0800

    Added post posting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   devise_for :caregivers
   devise_for :admins
   devise_for :users
+
+  authenticated :user do
+    root to: "users#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "static_pages#home", as: :unauthenticated_root
+  end
+
+  resources :users, only: [:show]
+  resources :posts, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+User.create!(name:  "Admin User",
+             email: "adminuser@thejourney.you",
+             password:              "password",
+             password_confirmation: "password")
+
+User.create!(name:  "Prima ry User",
+            email: "prima@thejourney.you",
+            password:              "password",
+            password_confirmation: "password")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@thejourney.you"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.posts.create!(content: content) }
+end
