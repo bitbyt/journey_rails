b45bc4c41be43b79f99cdb2e4b2266f1e0af9f32
Luqman Mohamed <lukas.woodman@gmail.com>
2016-08-05 09:52:54 +0800

    it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,9 @@ end
 
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.posts.create!(content: content, mood: "neutral") }
+  content = Faker::Hipster.paragraph(3)
+  mood = [ "happy", "neutral", "anxious", "sad", "lethargic" ].shuffle.sample
+  users.each { |user| user.posts.create!(content: content, mood: mood) }
 end
 
 User.create!(name:  "Dr Ellen Enders",
